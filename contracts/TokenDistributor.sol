91f1a5a2694fd614d66d34add623f6d6d90d8cc1
Zachary <1734015+nopslip@users.noreply.github.com>
2021-04-27 10:23:01 -0600

    removing unused return value on _delegateTokens

--- contracts/TokenDistributor.sol
+++ contracts/TokenDistributor.sol
@@ -263,12 +263,8 @@ contract TokenDistributor{
     * @notice execute call on token contract to delegate tokens   
     * @return boolean true on success 
     */
-    function _delegateTokens(address delegator, address delegatee) private returns (bool) {
+    function _delegateTokens(address delegator, address delegatee) private {
          GTCErc20  GTCToken = GTCErc20(token);
          GTCToken.delegateOnDist(delegator, delegatee);
-         return true;  
     } 
-
-
-
 }
